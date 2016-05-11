a03d502aba49eec884616f38b40c2bc0671bb043
Tim Watson <tswatson123@gmail.com>
2016-05-11 14:59:46 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+echo "Installing dependancies"
+sudo apt-get -y install vim build-essential cmake python-dev python3-dev
+
+echo "Setting up awesome vim bundle"
+cp .vim_runtime ~/.vim_runtime -r
+cd ~/.vim_runtime
+sh install_awesome_vimrc.sh
+
+echo "Fetching vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installing vundle plugins"
+vim +PluginInstall +qall
+
+echo "Finished"
